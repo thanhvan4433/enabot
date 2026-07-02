3c18f8308cb93c1508a5d7b181725554a9d08ee3
thanhvan4433 <van.pt0404@gmail.com>
2026-07-02 18:00:27 +0700

    update

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -19,7 +19,7 @@ function Newsletter() {
             <input
               id="name"
               type="text"
-              placeholder="Họ và tên (tùy chọn)"
+              placeholder="Họ và tên"
               className="w-full border border-gray-200 rounded-md px-3 py-2 text-gray-900 focus:ring-2 focus:ring-blue-500"
             />
 
